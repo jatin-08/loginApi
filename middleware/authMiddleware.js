e6db9beaa152456c931271ade96c219d23f3eb2d
Jatin Yadav <jatin.asmadiya@gmail.com>
2023-08-25 10:52:51 +0530

    intial commit

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,8 +7,10 @@ const loginModel = require("../model/LoginAPI");
 module.exports = asyncHandler(async (req, res, next) => {
     let token = req.headers['x-access-token'] || req.headers['authorization'];
     // console.log(authHeader);
-    token = token.replace(/^Bearer\s+/, "");
-    console.log(token);
+    if (token.startsWith("Bearer ")) {
+        token = token.split(" ")[1];
+        console.log(token);
+    }
 
     if (token) {
         jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
